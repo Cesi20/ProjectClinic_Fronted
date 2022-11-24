7f37bb1e02202d591dbbd9621edcb6e99b461276
cesia <ctitoh@autonoma.edu.pe>
2022-11-23 22:21:28 -0500

    NewUpdate

--- src/services/user.js
+++ src/services/user.js
@@ -4,3 +4,5 @@ export const authLoginDB = async (dataLogin) => {
   const { data } = await ClinicAPI.post("/auth/login", dataLogin);
   return data;
 };
+
+
--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import styles from "../../styles/auth.module.css";
+import styles from "../../styles/AuthForm.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 
