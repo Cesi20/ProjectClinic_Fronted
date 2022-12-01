100a14c2106f7463959ccb2b761f7fa3d524fd12
DanielaCh2 <dchoquehuanca@autonoma.edu.pe>
2022-12-01 11:11:57 -0500

    newupdate

--- src/hooks/useCertificado.js
+++ src/hooks/useCertificado.js
@@ -1,9 +1,9 @@
 import { CreateExamDB } from "../services/certificado";
 
 export const useCertificado = () => {
-    const CreateExam = async (id) => {
+    const CreateExam = async (id, dataExam) => {
         try {
-            const data = await CreateExamDB(id);
+            const data = await CreateExamDB(id, dataExam);
             const token = localStorage.getItem("token");
             return { ok: true, Exam: data.data, msg: data.msg };
         } catch (error) {
--- src/views/home/CrearCerti.jsx
+++ src/views/home/CrearCerti.jsx
@@ -1,36 +1,52 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useCertificado } from "../../hooks/useCertificado";
 import styles from "../../styles/AuthForm.module.css";
 
-export default function CrearCerti() {
-    const params = useParams();
-    const {CreateExam} = useCertificado();
-async function onSendEmail(e) {
-    e.preventDefault();
-    const {data, msg} = await CreateExam(params.id);
-    console.log(data);
-    console.log(msg);
+const dataExamPrint = {
+    date: "",
+    method: "",
+    result: "",
+    service: "",
+    sample: "",
 };
 
+export default function CrearCerti() {
+    const [dataExam, setDataExam] = useState(dataExamPrint);
+    const params = useParams();
+    const navigate = useNavigate();
+    const { CreateExam } = useCertificado();
+    async function onSendEmail(e) {
+        e.preventDefault();
+        const { data, msg } = await CreateExam(params.id, dataExam);
+        console.log(data);
+        console.log(msg);
+
+        if (dataExam) {
+            return navigate("/verPaciente");
+        }
+    };
+   
+
+    console.log(dataExam);
     return (
         <div className={styles.auth}>
             <div></div>
             <div className={styles["auth-form"]}>
-            <form className={styles.form} onSubmit={onSendEmail}>
-                <h1>Crear Certificado</h1>
-                <input type="date"/>
-                <input type="text" placeholder="Metodo"/>
-                <select>
-                    <option value="">Resultado</option>
-                    <option value="Positivo">Positivo</option>
-                    <option value="Negativo">Negativo</option>
-                </select>
-                <input type="text" placeholder="Servicios"/>
-                <input type="text" placeholder="Sample"/>
-                <button type="submit"> Enviar Certificado</button>
-                
-            </form>
+                <form className={styles.form} onSubmit={onSendEmail}>
+                    <h1>Crear Certificado</h1>
+                    <input type="date" onChange={(e) => setDataExam({ ...dataExam, date: e.target.value })} />
+                    <input type="text" placeholder="Metodo" onChange={(e) => setDataExam({ ...dataExam, method: e.target.value })} />
+                    <select onChange={(e) => setDataExam({ ...dataExam, result: e.target.value })}>
+                        <option value="">Resultado</option>
+                        <option value="Positivo">Positivo</option>
+                        <option value="Negativo">Negativo</option>
+                    </select>
+                    <input type="text" placeholder="Servicios" onChange={(e) => setDataExam({ ...dataExam, service: e.target.value })} />
+                    <input type="text" placeholder="Sample" onChange={(e) => setDataExam({ ...dataExam, sample: e.target.value })} />
+                    <button type="submit"> Enviar Certificado</button>
+
+                </form>
             </div>
         </div>
     )
